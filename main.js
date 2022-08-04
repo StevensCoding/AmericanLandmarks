d617e7831306636e64f90b0e52286891cebf8d3f
Steven <unclearwillo@gmail.com>
2022-08-04 19:18:54 -0400

    Added button functionality

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-const magic = document.getElementById("landmarkButton");
-magic.addEventListener("click", (theMagic) => {
-    theMagic.preventDefault()
-    theMagic.target[0].value
-    fetch("")
-    .then(res => res.json())
-    .then(res => console.log(res))
-})
+document.addEventListener("DOMContentLoaded", () => {
+    const magic = document.getElementById("landmarkButton");
+    magic.addEventListener("click", (e) => {
+        fetch(`http://localhost:3000/landmarks`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+              }
+        })
+        .then(res => res.json())
+        .then(data => {
+            data.map(names => {
+                console.log(names);
+                const h3 = document.createElement("h3");
+                const para = document.createElement("p");
+                const img = document.createElement("img");
+                const location = names.location[0];
+                const namesList = document.querySelector("#forLandmarks");
+                h3.textContent = names.landmarkName;
+                img.src= `${names.url}`
+                img.height = 400;
+                img.width = 400;
+                para.textContent = `Located in: ${location.city}, ${location.state}`;
+                namesList.append(h3, img, para);
+
+            })})
+        })
+    })
