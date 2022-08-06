9cc9f2fb620f3f1450114dd94c0bcb2e43871678
Steven <unclearwillo@gmail.com>
2022-08-06 15:43:12 -0400

    Added fetch to submit data to db.json

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(res => res.json())
         .then(data => {
             data.map(names => {
-                console.log(names);
+                
                 const h3 = document.createElement("h3");
                 const para = document.createElement("p");
                 const img = document.createElement("img");
@@ -22,8 +22,33 @@ document.addEventListener("DOMContentLoaded", () => {
                 img.height = 400;
                 img.width = 400;
                 para.textContent = `Located in: ${location.city}, ${location.state}`;
-                namesList.append(h3, img, para);
 
+                if (location.city == "NA") {
+                    para.innerText = `Located in: ${location.state}`;
+                } else if (location.state == "NA"){
+                    para.innerText = `Located in: ${location.city}`;
+                }else {
+                    para.innerText = `Located in: ${location.city}, ${location.state}`;
+                }
+
+                namesList.append(h3, img, para);
+                // console.log(para.textContent)
             })})
+        })
+        const submitButton = document.getElementById("submitSuggestion")
+        submitButton.addEventListener('click', e => {
+            e.preventDefault();
+            fetch(`http://localhost:3000/suggestions`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                  }
+
+            })
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+            const sugBox = document.getElementById("suggestionsBox")
+            sugBox.innerHTML = 'Thank you for your suggestion! It has been submitted.'
         })
     })
